dbb96e67da345d5dff61a3421294826cc0bdebe0
Gega Oragvelidze <gegaoragvelidze97@gmail.com>
2024-06-02 15:43:38 +0400

    .

---
+++ SabininaDummy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SabininaDummy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
